e434d2eedab0e882ce1eeba417b35011670ea435
jogaudard <joseph.gaudard@pm.me>
2025-03-22 00:40:27 +0100

    test passed

--- building/dev.R
+++ building/dev.R
@@ -2,3 +2,14 @@ available::available("licoread")
 
 package_path = "/home/jga051/Documents/01_PhD/06_projects/licoread"
 usethis::create_package(path = package_path)
+
+usethis::use_test(name = "read_81x")
+
+# build the documentation
+devtools::document()
+
+# check the examples work
+devtools::run_examples()
+
+# check the tests work
+devtools::test()
