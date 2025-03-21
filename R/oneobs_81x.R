0db2c819b9be7b287e0f53727dca67a446d59b8b
jogaudard <joseph.gaudard@pm.me>
2025-03-21 23:44:30 +0100

    map2 works

--- R/oneobs_81x.R
+++ R/oneobs_81x.R
@@ -1,9 +1,47 @@
 
 
 oneobs_81x <- function(
-    oneobs,
+    # file,
+    # all_obs,
     start,
     end
 ) {
+one_obs <- all_obs[start:end]
 
+# location of meta and data and col names
+
+first_el <- substr(one_obs, start = 1, stop = 1)
+
+meta_loc <- as.numeric(first_el) |>
+is.na() |>
+which()
+
+data_loc <- which(!is.na(as.numeric(first_el))) # location of data row are where as.numeric did not produce NA
+col_names_loc <- min(data_loc) - 1 # row with col names  is just before the first numeric row
+data_loc <- append(data_loc, col_names_loc, 0)
+
+# read data
+line_skip <- min(data_loc) - 1 + start # we want to skip everything before the colnames
+line_max <- max(data_loc) - line_skip - 1 # skipping everything after the last row of data
+
+# read tsv is longer, but much safer than re using the one_obs list (in case there are empty cells)
+obs_data <- read_tsv(file, skip = line_skip, n_max = line_max)
+
+# read meta data
+
+metadata <- read_yaml(text = one_obs[-data_loc])
+
+table_meta <- enframe(metadata) |> # make a df out of the list
+pivot_wider(names_from = name, values_from = value) |> # each element of the list is now a column
+unnest(names(metadata))
+
+# one_obs_tbl <- tibble(
+#     data = obs_data,
+#     table_meta
+# )
+
+one_obs_tbl <- bind_cols(table_meta, obs_data) |>
+nest(data = names(obs_data))
+
+one_obs_tbl
 }
--- R/read_81x.R
+++ R/read_81x.R
@@ -3,7 +3,7 @@ library(yaml)
 
 # use readlines to create a list
 file <- "raw_files/10-28-2011.81x"
-single_file <- "raw_files/test.81x"
+# single_file <- "raw_files/test.81x"
 # full_file <- readLines(file)
 raw_lines <- read_lines_raw(file)
 # use the empty line as a slicer
@@ -12,5 +12,40 @@ zero_lines <- which(length == 0)
 
 all_obs <- read_lines(file)
 
-one_obs <- all_obs[0:(zero_lines[1] - 1)]
+start_vec <- c(0, (zero_lines[-length(zero_lines)] + 1))
+end_vec <- zero_lines
 
+# extractin the first batch of obs
+# start <- 0
+# end <- (zero_lines[1] - 1)
+# one_obs <- all_obs[start:end]
+
+# oneobs_81x(file, all_obs, start_vec[1], end_vec[1]) |> str()
+
+# data_file <- tibble()
+
+# for (i in 1:length(zero_lines)) {
+
+#   data_file[i] <- oneobs_81x(file, all_obs, start_vec[i], end_vec[i])
+
+
+# }
+# data_file
+
+# test <- tibble(
+#     start_vec,
+#     end_vec
+# ) |>
+# mutate(
+#     data = map(all_obs, oneobs_81x, file = file, all_obs = all_obs, start = start_vec, end = end_vec)
+# )
+
+# map(all_obs, oneobs_81x, file = file, all_obs = all_obs, start = start_vec, end = end_vec)
+# map2(start_vec, end_vec, oneobs_81x, start = start_vec, end = end_vec, file = file, all_obs = all_obs)
+
+# oneobs_81x(start_vec[1], end_vec[1])
+output <- map2(start_vec, end_vec, oneobs_81x)
+
+class(output)
+str(output)
+View(output)
