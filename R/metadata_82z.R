cfe556905ca9ebe431a2c4a53558df531199159a
jogaudard <joseph.gaudard@pm.me>
2025-04-09 16:27:34 +0200

    preparing metadata

--- R/metadata_82z.R
+++ R/metadata_82z.R
@@ -6,5 +6,33 @@
 metadata_82z <- function(
     filepath
 ) {
-    asdf
+    metadata_raw <- unz(filepath, "metadata.json") |>
+        fromJSON() |>
+        enframe(unlist()) |>
+  mutate(
+    # add an id col to group per gas
+    group = str_extract(name, "\\d$"),
+    name = str_remove(name, "\\d$")
+  )
+
+# nb of column to separate
+rgx_split <- "\\."
+n_cols_max <-
+  metadata_raw |>
+  pull(name) |>
+  str_split(rgx_split) |>
+  map_dbl(~length(.)) |>
+  max()
+
+last_col <- paste0("name", n_cols_max)
+
+nms_sep <- paste0("name", 1:n_cols_max)
+metadata <-
+  metadata_raw |>
+  separate(name, into = nms_sep, sep = rgx_split, fill = "right") |>
+  mutate(
+    {{last_col}} := replace_na({{last_col}}, "VALUE")
+  ) |>
+    pivot_wider(names_from = {{last_col}}, values_from = "value")
+
 }
--- building/tries82z.R
+++ building/tries82z.R
@@ -1,8 +1,59 @@
 library(tidyverse)
 library(jsonlite)
+library(measurements)
 
-filepath <- "local_stuff/20240725_sample_data_all_gases/82m-0109-20240725000057.82z/metadata.json"
+filepath <- "local_stuff/20240725_sample_data_all_gases/82m-0109-20240725000057.82z"
 
-metadata_file <- unz("local_stuff/20240725_sample_data_all_gases/82m-0109-20240725000057.82z", "metadata.json")
+metadata_file <- unz(filepath, "metadata.json")
 
-fromJSON(metadata_file)
+metadata <- fromJSON(metadata_file)
+
+# as_tibble(metadata$FLUX)
+# str(metadata)
+
+# parse_json(metadata_file)
+# we are getting somewhere with that one
+
+metadata_raw <- enframe(unlist(metadata)) |>
+  mutate(
+    # add an id col to group per gas
+    group = str_extract(name, "\\d$"),
+    name = str_remove(name, "\\d$")
+  )
+
+# https://www.r-bloggers.com/2018/10/converting-nested-json-to-a-tidy-data-frame-with-r/
+rgx_split <- "\\." # this step to get the number of cols needed, will have to be automated
+n_cols_max <-
+  metadata_raw %>%
+  pull(name) %>% 
+  str_split(rgx_split) %>% 
+  map_dbl(~length(.)) %>% 
+  max()
+n_cols_max
+
+nms_sep <- paste0("name", 1:n_cols_max)
+metadata_sep <-
+  metadata_raw %>% 
+  separate(name, into = nms_sep, sep = rgx_split, fill = "right") |>
+  mutate(
+    name3 = replace_na(name3, "VALUE")
+  ) |>
+    pivot_wider(names_from = "name3", values_from = "value")
+View(metadata_sep)
+
+# metadata_sep |>
+# select(name3) |>
+# distinct()
+
+# now we should convert the values in the units we want for Fluxible
+# that is a problem for later
+# metadata_conv <- metadata_sep |>
+#     mutate(
+#         unit_from = str_remove(UNITS, "\\W"),
+#         dimension = str_extract(UNITS, "\\d$"),
+#         VALUE = case_when(
+#         dimension == 2 ~ conv_unit(VALUE, unit_from, "m2"),
+#         dimension == 3 ~ conv_unit(VALUE, unit_from, "l")
+#         )
+#     )
+# metadata_conv
