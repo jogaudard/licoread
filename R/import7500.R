97365bc4377e861d431f47446c70f44cc5d77d60
jogaudard <joseph.gaudard@pm.me>
2025-07-31 10:16:13 +0200

    strucuture

---
+++ R/import7500.R
@@ -0,0 +1,12 @@
+#' imports data from li7500 setup
+#' @description 
+#' @param 
+#' 
+#' 
+
+
+import7500 <- function(path,
+                       version = "pre2023",
+                       plotinfo = TRUE) {
+                        
+                       }
